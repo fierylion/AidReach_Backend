56fafaaf72b13e0d563dba7d009c84e3a029086a
fierylion <mawalladaniel2018@gmail.com>
2023-08-03 04:28:14 +0300

    testing

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-console.log('hi')
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+}
+)
+const port =process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+}
+)
